184ebc49e3e08460a0893c08eea815bb40dcac59
Thantip Setthakit <thantip.st@hotmail.com>
2018-06-30 13:31:08 +0700

    hello github

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import SayHello from "./components/SayHello";
+import Border from "./components/Border";
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "Thantip Setthakit",
+      age: 23
+      
+    };
+  }
+  incrementAge(){
+    this.setState({age: this.state.age +10 });
+  }
+  decrementAge(){
+    this.setState({age: this.state.age -10 });
+  }
+
+  render() {
+    return (
+     <div className="App">
+       <SayHello name={this.state.name} age={this.state.age}/>
+       <p>age, {this.state.age}</p>
+    <div
+           style={{
+           width: this.state.age,
+           border: 2,
+           borderStyle: "solid",
+           color: "#deab53"
+         }}
+       />
+    <Border>
+    <button onClick={() => this.decrementAge()}>ลดอายุ</button>
+    <button onClick={() => this.incrementAge()}>เพิ่มอายุ</button>
+    </Border>
+    </div>
+    );
+  }
+}
+
+export default App;
